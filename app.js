37a6f48b1f46b88542cce7fb4e53264b9fc079ed
MHusain22 <husain.vijapura@gmail.com>
2024-08-05 10:17:24 +0530

    Mouse cursor Animations Added

--- app.js
+++ app.js
@@ -65,3 +65,9 @@ window.onscroll = () => {
   });
 };
 
+//Mouse cursor
+var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
